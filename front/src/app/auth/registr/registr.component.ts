a2e366135d62eda4df6909926a651f62958f9452
k.popkov@bk.ru <8821022qweasd>
2021-06-17 22:54:17 +0300

    regist

---
+++ front/src/app/auth/registr/registr.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-registr',
+  templateUrl: './registr.component.html',
+  styleUrls: ['./registr.component.scss']
+})
+export class RegistrComponent implements OnInit {
+
+  public registerForm: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.registerForm = this.formBuilder.group({
+      email: ['k.popkov@bk.ru', [Validators.required, Validators.email]],
+      password: ['2', Validators.required],
+      rePassword: ['2', Validators.required],
+      name: ['2', Validators.required],
+      number: ['2', Validators.required]
+    });
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  logForm() {
+
+    this.router.navigate(['auth/first-setting']);
+
+  }
+}
