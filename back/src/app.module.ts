f8ef000ee468f8a0ea9d87b0664aac65e1a9cd17
k.popkov@bk.ru <8821022qweasd>
2021-06-19 01:16:17 +0300

    back

---
+++ back/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { User } from './users/users.model';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      models: [User],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ back/src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './users.model';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectModel } from '@nestjs/sequelize';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
+
+  async createUser(dto: CreateUserDto) {
+    const user = await this.userRepository.create(dto);
+    return user;
+  }
+  async getAllUsers() {
+    const users = this.userRepository.findAll();
+    return users;
+  }
+}
