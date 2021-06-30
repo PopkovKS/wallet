93da551852d0f18da78abb43576aa42c17b2cb98
k.popkov@bk.ru <8821022qweasd>
2021-06-30 23:03:31 +0300

    update

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,6 +3,8 @@ import { User } from './users/users.model';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { RolesModule } from './roles/roles.module';
+import { Role } from './roles/roles.model';
 
 @Module({
   imports: [
@@ -16,10 +18,11 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DB,
-      models: [User],
+      models: [User, Role],
       autoLoadModels: true,
     }),
     UsersModule,
+    RolesModule,
   ],
   controllers: [],
   providers: [],
--- back/src/users/users.model.ts
+++ back/src/users/users.model.ts
@@ -1,4 +1,13 @@
-import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from '../roles/roles.model';
+import { UserRoles } from '../roles/user-roles.model';
 
 interface UserCreationAttrs {
   email: string;
@@ -7,6 +16,7 @@ interface UserCreationAttrs {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttrs> {
+  @ApiProperty({ example: '1', description: 'Уникальный индификатор' })
   @Column({
     type: DataType.INTEGER,
     unique: true,
@@ -14,7 +24,7 @@ export class User extends Model<User, UserCreationAttrs> {
     primaryKey: true,
   })
   id: number;
-
+  @ApiProperty({ example: 'user@mail.ru', description: 'Почтовый адрес' })
   @Column({
     type: DataType.STRING,
     unique: true,
@@ -22,9 +32,13 @@ export class User extends Model<User, UserCreationAttrs> {
   })
   email: string;
 
+  @ApiProperty({ example: '12345', description: 'Пароль пользователя' })
   @Column({
     type: DataType.INTEGER,
     allowNull: false,
   })
   password: string;
+
+  @BelongsToMany(() => Role, () => UserRoles)
+  roles: User[];
 }
