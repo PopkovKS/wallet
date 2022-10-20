aba0f939d31ec174c42fadcf852f66d4164ab1cc
k.popkov@bk.ru <8821022qweasd>
2022-10-20 19:59:18 +0300

    update

---
+++ front/src/app/pages/first-setting/first-setting.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FirstSettingComponent } from './first-setting.component';
+
+describe('FirstSettingComponent', () => {
+  let component: FirstSettingComponent;
+  let fixture: ComponentFixture<FirstSettingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FirstSettingComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FirstSettingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ front/src/app/pages/first-setting/first-setting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-setting',
+  templateUrl: './first-setting.component.html',
+  styleUrls: ['./first-setting.component.scss']
+})
+export class FirstSettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
